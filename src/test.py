e3af6854e1da68849a2106418d4df63d74e8d821
Elijah Salberg <esalberg@uccs.edu>
2019-05-10 12:51:34 -0600

    Now only shows top rated movies for better results

--- src/test.py
+++ src/test.py
@@ -1,7 +1,8 @@
 import requests
 import json
 
-movie = input("What's a movie you like? ")
+
+movie = input("What's a movie you like: ")
 
 url = "https://api.themoviedb.org/3/search/movie?include_adult=false&query=" + movie + "&language=en-US&api_key=d85618974402c12dd65393c379cfe785"
 
@@ -19,10 +20,12 @@ response = requests.request("GET", url, data=payload)
 
 data = response.json()
 
-print ("Here are some movies you might like!\n")
+total = 0
 
-print (data['results'][0]['title'] + "\n")
+for i in data['results']:
+    if i['vote_average'] > 7.5:
+        total += 1
+        print ("\n" + i['title'] + "\n")
 
-print (data['results'][1]['title'] + "\n")
-
-print (data['results'][2]['title'])
+if total == 0:
+    print ("\nI didn't find any good movies like that one.\n")
